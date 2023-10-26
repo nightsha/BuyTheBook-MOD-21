a529b3763a042483a1549a142a6582cdde2a9aaf
nightsha <130249622+nightsha@users.noreply.github.com>
2023-10-26 13:46:45 -0500

    updatedAuth.jsFile

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,5 @@
+// refactored and updated code to use graphQl API
+// used chatGPT help
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -5,8 +7,10 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  // function for our authentication/made changes here
+  authMiddleware: function (context) {
+    const { req } = context;
+
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -16,24 +20,48 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      // made changes
+      throw new Error ( 'You have no token!' );
     }
 
     // verify token and get user data out of it
+    // changes made here
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      const { data } = jwt.verify(token, secret, { expiresIn: expiration });
+      context.user = data;
+    } catch (error) {
+      throw new Error('Invalid token');
+      
     }
 
-    // send to next endpoint
-    next();
+    // return context instead of next
+    return context;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
+
+
+
+
+
+// additional code to work with not sure where to file this block
+const { authMiddleware } = require('./auth'); // Import your authentication middleware
+
+const resolvers = {
+  Query: {
+    // Apply authMiddleware before resolving this query
+    protectedQuery: (parent, args, context) => {
+      // The user is available in context after authentication
+      const user = context.user;
+      if (!user) {
+        throw new Error('Authentication required');
+      }
+
+      // Your resolver logic here
+    },
   },
 };
