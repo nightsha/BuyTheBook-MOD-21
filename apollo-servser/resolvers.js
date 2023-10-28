cada15c437c441aa1b2d103618141ffd1e8a5c02
nightsha <130249622+nightsha@users.noreply.github.com>
2023-10-27 22:07:30 -0500

    addedMoreFunctionToTypedefs

--- apollo-servser/resolvers.js
+++ apollo-servser/resolvers.js
@@ -19,6 +19,32 @@ const resolvers = {
       }   
      
      }
+     addUser: (parent, args, context, info) => {
+      // Your addUser logic here
+      // Check if email is unique
+      // Hash the password
+      // Create a new user record
+      // Return an authentication token or user object
+    },
+    login: (parent, args, context, info) => {
+      // Your login logic here
+      // Find the user by email
+      // Verify the password
+      // Return an authentication token or user object
+    },
+    saveBook: (parent, args, context, info) => {
+      // Your saveBook logic here
+      // You can access the book parameters using args.bookInput
+      // Check authentication
+      // Create a new book record for the user
+      // Return a confirmation or user object
+    },
+    removeBook: (parent, args, context, info) => {
+      // Your removeBook logic here
+      // Check authentication
+      // Remove the specified book from the user's list
+      // Return a confirmation or user object
+    },
     
 
   }
@@ -29,23 +55,23 @@ module.exports = resolvers;
 
 
 // additional steps to take
-Schema Definition:
+// Schema Definition:
 
-In your GraphQL schema, you should define the "createClass" mutation to match the resolver you've just created. This should be done where you define your schema, typically in a separate schema file.
+// In your GraphQL schema, you should define the "createClass" mutation to match the resolver you've just created. This should be done where you define your schema, typically in a separate schema file.
 
-graphql
-Copy code
-type Mutation {
-  createClass(input: ClassInput!): Class
-}
-Make sure you have a corresponding "ClassInput" type defined in your schema for the input object.
+// graphql
+// Copy code
+// type Mutation {
+//   createClass(input: ClassInput!): Class
+// }
+// Make sure you have a corresponding "ClassInput" type defined in your schema for the input object.
 
-Import and Use Your Resolvers:
+// Import and Use Your Resolvers:
 
-Finally, you need to import and use these resolvers in your GraphQL server setup. How you do this depends on the technology stack you're using, but 
-typically it involves importing your resolvers and combining them with your schema definition.
+// Finally, you need to import and use these resolvers in your GraphQL server setup. How you do this depends on the technology stack you're using, but 
+// typically it involves importing your resolvers and combining them with your schema definition.
 
-With these steps, 
-you can define query and mutation functionality to work with your Mongoose models in your GraphQL server. 
-Adjust the mutation resolvers and schema definitions 
-as needed for your specific use case and model operations.
+// With these steps, 
+// you can define query and mutation functionality to work with your Mongoose models in your GraphQL server. 
+// Adjust the mutation resolvers and schema definitions 
+// as needed for your specific use case and model operations.
--- apollo-servser/schemas/typeDefs.js
+++ apollo-servser/schemas/typeDefs.js
@@ -4,13 +4,51 @@ const typeDefs = gql`
   type Class {
     _id: ID
     name: String
-    building: String
+      building: String
     creditHours: Int
+  }
+  type Profile {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    skills: [String]!
+  }
+
+  type Auth {
+    token: ID!
+    profile: Profile
   }
 
   type Query {
     classes: [Class]
+    profiles: [Profile]!
+    profile(profileId: ID!): Profile
+    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
+    me: Profile
+  }
+
+  type input BookInput {
+    author: [String]!
+    description: String!
+    title: String!
+    bookID: ID!
+    image: String
+    link: String
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookInput: BookInput!): User
+    removeBook(bookID: ID!): User
+
+    addSkill(profileId: ID!, skill: String!): Profile
+    removeProfile: Profile
+    removeSkill(skill: String!): Profile
   }
 `;
+
+ 
 
 module.exports = typeDefs;
