69ac43f13a65befe3ccd45e5278a9ece1c14bbfb
nightsha <130249622+nightsha@users.noreply.github.com>
2023-10-31 21:24:31 -0500

    mutatedHandleFormSubmit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BuyTheBook-MOD-21
 A search engine to purchase books
+
+![A picture of a book search engine](<Screenshot 2023-10-31 204712.png>)
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -40,7 +40,10 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await client.mutate({
+        mutation: ADD_USER,
+        variables: userFormData,
+      })
 
       if (!response.ok) {
         throw new Error('something went wrong!');
